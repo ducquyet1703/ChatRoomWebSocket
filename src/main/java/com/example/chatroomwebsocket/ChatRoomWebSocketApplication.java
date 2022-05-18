2d3df8a2ddf28188b05af81cd700997b3ee5062c
ducquyet <gunnynhoc7@gmail.com>
2022-05-18 14:48:55 +0700

    Fisrt Commit

---
+++ src/main/java/com/example/chatroomwebsocket/ChatRoomWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatroomwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRoomWebSocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatroomwebsocket/ChatRoomWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatroomwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRoomWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
